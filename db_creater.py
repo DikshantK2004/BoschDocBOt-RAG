9ede8f469b062a8a7090e595e58b59c1290aa880
Arushi Garg <arushigarg@Arushis-MacBook-Pro.local>
2024-05-19 23:32:35 +0530

    readme updated

--- db_creater.py
+++ db_creater.py
@@ -3,8 +3,8 @@ import os
 
 # list ur pdfs in a folder and pass the path to the function
 #example 
-pdf_path = os.listdir('/Users/arushigarg/Desktop/bosch/pdfs')
-pdf_path = ['/Users/arushigarg/Desktop/bosch/pdfs/' + path for path in pdf_path if path.endswith('.pdf')]
+pdf_path = os.listdir('/path/to/your/pdfs/folder')
+pdf_path = ['/path/to/your/pdfs/folder/' + path for path in pdf_path if path.endswith('.pdf')]
 load_and_process_documents(pdf_path)
 
 # /path/to/your/pdfs/folder/
